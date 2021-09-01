6b399257169b771076bb0b2414b5ffd44b41afbf
Pausbrak <rslego@gmail.com>
2021-09-01 19:02:45 -0400

    Source

---
+++ Source/Code/HarmonyPatches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PredictableDrills
+{
+    [StaticConstructorOnStartup]
+    class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("pausbrak.predictabledeepdrills");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(DeepDrillUtility))]
+    [HarmonyPatch("GetBaseResource", new Type[] { typeof(Map), typeof(IntVec3)})]
+    static class Patch_PathFinder_FindPath
+    {
+        static ThingDef Postfix(ThingDef resource, Map map, IntVec3 cell)
+        {
+            // Don't drop stones if we wouldn't be dropping them anyway
+            if (resource == null) return null;
+
+            // Default to whatever vanilla generated if we can't find a proper chunk type
+            return cell.GetTerrain(map).GetMapChunk(map) ?? resource;
+        }
+    }
+}
---
+++ Source/Code/StoneFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Verse;
+
+namespace PredictableDrills
+{
+    public static class StoneFinder
+    {
+        /// <summary>
+        /// If this terrain is a natural stone type for the given map, returns the chunk associated with it
+        /// Otherwise returns null.
+        /// </summary>
+        /// <returns>The chunk.</returns>
+        /// <param name="terrain">Terrain.</param>
+        public static ThingDef GetMapChunk(this TerrainDef terrain, Map map)
+        {
+            return Find.World.NaturalRockTypesIn(map.Tile)
+                    .FirstOrDefault(r =>
+                    {
+                        return terrain == r.building.leaveTerrain
+                            || terrain == r.building.naturalTerrain
+                            || terrain == r.building.leaveTerrain.smoothedTerrain
+                            || terrain == r.building.naturalTerrain.smoothedTerrain;
+                    });
+        }
+    }
+}
